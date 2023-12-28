4556ea2087cfe3922bde500fc31d43117e524478
januridp <hello@januridp.com>
2023-12-29 02:26:57 +0700

    add shell script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1
+docker build -t karsajobs:latest .
+
+#Perintah untuk melihat daftar image di lokal
+docker images
+
+#Perintah untuk mengubah nama image agar sesuai dengan format Docker Hub
+docker tag karsajobs:latest januridp/karsajobs:latest
+
+#Perintah untuk login ke Docker Hub
+USERNAME="januridp"
+read -s -p "Enter Docker Hub password: " PASSWORD
+echo "$PASSWORD" | docker login -u $USERNAME --password-stdin
+unset PASSWORD
+
+#Perintah untuk mengunggah image ke Docker Hub
+docker push januridp/karsajobs:latest
